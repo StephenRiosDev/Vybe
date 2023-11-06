1d3ee8e9ccb5264848c026cc0c793790b136ef74
StephenRiosDev <StephenRiosDev@gmail.com>
2023-11-06 09:28:10 -0600

    Added login/register switch and inputs

--- src/app/components/loginForm/loginForm.component.tsx
+++ src/app/components/loginForm/loginForm.component.tsx
@@ -7,14 +7,32 @@ import { Visibility } from '@mui/icons-material';
 export const LoginForm = () => {
 
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [passwordConfirmVisible, setPasswordConfirmVisible] = useState(false);
+  const [isRegister, setIsRegister] = useState(false);
 
   return (
     <>
       <Grid container spacing={6} flexDirection="column" alignItems="center" minWidth="100%">
 
         <Grid item minWidth="100%">
-          <TextField label="Username" />
+          <TextField label={ isRegister ? "Desired Username" : "Username"} name="user-name" />
         </Grid>
+
+        { isRegister && 
+          <>
+            <Grid item minWidth="100%">
+              <TextField label="Email" name="email" />
+            </Grid>
+
+            <Grid item minWidth="100%">
+              <TextField label="First Name" name="first-name" />
+            </Grid>
+
+            <Grid item minWidth="100%">
+              <TextField label="Last Name" name="last-name" />
+            </Grid>
+          </>
+        }
 
         <Grid item minWidth="100%">
           <TextField label="Password" type={ passwordVisible ? 'text' : 'password'} InputProps={{
@@ -25,12 +43,28 @@ export const LoginForm = () => {
           }} />
         </Grid>
 
+        { isRegister &&
+          <Grid item minWidth="100%">
+            <TextField label="Confirm Password" type={ passwordConfirmVisible ? 'text' : 'password'} InputProps={{
+              endAdornment: 
+                <InputAdornment position="end" onClick={ e => setPasswordConfirmVisible(!passwordConfirmVisible)}>
+                  <Visibility />
+                </InputAdornment>,
+            }} />
+          </Grid>
+        }
+
         <Grid item container flexDirection="row-reverse" justifyContent="space-between">
           <Grid item>
             <Button variant="vybe-right">Let's Vibe</Button>
           </Grid>
           <Grid item>
-            <Button variant="vybe-left" color="secondary">Register</Button>
+            <Button 
+              variant={ isRegister ? "vybe-left" : "vybe"} color="secondary"
+              onClick={ e=> setIsRegister(!isRegister) }
+            >
+              { isRegister ? "Login" : "Register"}
+            </Button>
           </Grid>
         </Grid>
       </Grid>
