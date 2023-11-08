a0001dd1793c83bf55c4e5dbc2612c0b5f77f1bf
StephenRiosDev <StephenRiosDev@gmail.com>
2023-11-08 08:38:39 -0600

    stubbed out spotify service

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+import { User } from "../models/user";
+import { UserService } from './user.service';
+
+class _SpotifyService {
+
+  constructor(){};
+
+  
+}
+
+export const SpotifyService = new _SpotifyService();
+
+
